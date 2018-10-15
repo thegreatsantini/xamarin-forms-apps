14b170c4321398e854f88f4e46515ada0dd1b0d3
thegreatsantini <lss13@my.fsu.edu>
2018-10-15 12:08:34 -0700

    started first Xamarin forms app

---
+++ HelloWorld/HelloWorld/MyPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HelloWorld
+{
+    public partial class MyPage : ContentPage
+    {
+        public MyPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
