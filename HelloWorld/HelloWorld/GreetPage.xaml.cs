37c865c9c5aef717134bca9f27e293ac40ab1402
thegreatsantini <lss13@my.fsu.edu>
2018-10-15 21:27:04 -0700

    completed first section of Xamarin course

--- HelloWorld/HelloWorld/GreetPage.xaml.cs
+++ HelloWorld/HelloWorld/GreetPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 
 using Xamarin.Forms;
+using static Xamarin.Forms.Device;
 
 namespace HelloWorld
 {
@@ -10,6 +11,26 @@ namespace HelloWorld
         public GreetPage()
         {
             InitializeComponent();
+            slider.Value = 0.55; 
+            // one way of setting padding for differant palatforms
+            //double top;
+            //switch ( Device.RuntimePlatform )
+            //{
+            //    case Device.iOS:
+            //        top = 20;
+            //        break;
+            //    case Device.Android:
+            //    case Device.UWP:
+            //    default:
+            //        top = 0;
+            //        break;
+            //}
+            //Padding = new Thickness(5, top, 5, 0);
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            DisplayAlert("Title", "Hello World", "OK");
         }
     }
 }
