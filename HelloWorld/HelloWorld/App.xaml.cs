da370af1b6aaf33ae051674a5359e343716321f6
thegreatsantini <lss13@my.fsu.edu>
2018-10-16 21:26:10 -0700

    completed gid layout exercise 2

--- HelloWorld/HelloWorld/App.xaml.cs
+++ HelloWorld/HelloWorld/App.xaml.cs
@@ -11,7 +11,7 @@ namespace HelloWorld
         {
             InitializeComponent();
 
-            MainPage = new GridExercise1();
+            MainPage = new GridExercise2();
         }
 
         protected override void OnStart()
---
+++ HelloWorld/HelloWorld/GridLayoutExercise2.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HelloWorld
+{
+    public partial class GridLayoutExercise2 : ContentPage
+    {
+        public GridLayoutExercise2()
+        {
+            InitializeComponent();
+        }
+    }
+}
