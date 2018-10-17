83db6f3795bd176799552c5bcd6d2f22afce9100
thegreatsantini <lss13@my.fsu.edu>
2018-10-17 16:57:46 -0700

    reviewed Quotes app with clayton

--- Quotes_App/Quotes_App/MainPage.xaml.cs
+++ Quotes_App/Quotes_App/MainPage.xaml.cs
@@ -9,32 +9,63 @@ namespace Quotes_App
 {
     public partial class MainPage : ContentPage
     {
-        private string Quote1 = "Nothing is given everything is earned";
-        private string Quote2 = "I'm too drunk to taste this chicken";
-        private string Quote3 = "The winning team is the one with the most points";
-        int index = 0;
+        private const string Quote1 = "Nothing is given everything is earned";
+        private const string Quote2 = "I'm too drunk to taste this chicken";
+        private const string Quote3 = "The winning team is the one with the most points";
+        private List<string> QuotesList;
+        private Dictionary<string, string> QuotesDictionary = new Dictionary<string, string>();
+        private int index;
 
-        string[] QuotesList;
+        public void GetQuotes()
+        {
+            var quotesFromDatabase = new List<string>(){
+                Quote1,
+                Quote2,
+                Quote3
+            };
+
+            QuotesList = quotesFromDatabase;
+        }
+
         private void Handle_Clicked(object sender, System.EventArgs e)
         {
-            QuotesList = new string[] { Quote1, Quote2, Quote3 };
-            if ( index == QuotesList.Count() - 1 ) {
-                index = 0;
+            try
+            {
+                if (index == QuotesList.Count() - 1)
+                {
+                    index = 0;
+                }
+                else
+                {
+                    index++;
+                }
+
                 QuoteLabel.Text = QuotesList[index];
+                
+
             }
-            else {
-                index++;
-                QuoteLabel.Text = QuotesList[index];
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
             }
         }
+
+        /// <summary>
+        /// Test method
+        /// </summary>
+        /// <param name="quotes">Quotes.</param>
+        public void Derp(List<string> quotes)
+        {
+            QuotesList = quotes;
+        }
+
+
+
 
         public MainPage()
         {
             InitializeComponent();
-
-            //WHy do I have to repeat this line?
-            QuotesList = new string[] { Quote1, Quote2, Quote3 };
-
+            GetQuotes();
             QuoteLabel.Text = QuotesList[0];
 
 
@@ -48,5 +79,12 @@ namespace Quotes_App
             //};
 
         }
+    }
+
+    public class FamilyMember
+    {
+        int age { get; set; }
+        string name { get; set; }
+        string phoneNumber { get; set; }
     }
 }
