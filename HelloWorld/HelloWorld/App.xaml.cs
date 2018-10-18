7f5cd36ea8370299788523becbb6176384178cf7
thegreatsantini <lss13@my.fsu.edu>
2018-10-18 11:29:07 -0700

    completed relative layout section challenge

--- HelloWorld/HelloWorld/App.xaml.cs
+++ HelloWorld/HelloWorld/App.xaml.cs
@@ -11,7 +11,7 @@ namespace HelloWorld
         {
             InitializeComponent();
 
-            MainPage = new RelativeLayout();
+            MainPage = new RelativeExercise();
         }
 
         protected override void OnStart()
