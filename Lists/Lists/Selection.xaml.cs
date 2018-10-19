d9d0d22d11bb9f2e25c4ce4ca6dc59816fa51bf8
thegreatsantini <lss13@my.fsu.edu>
2018-10-19 15:14:44 -0700

    completed remove

--- Lists/Lists/Selection.xaml.cs
+++ Lists/Lists/Selection.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,27 +11,56 @@ namespace Lists
 {
     public partial class Selection : ContentPage
     {
-        void Handle_ItemSelected(object sender, Xamarin.Forms.SelectedItemChangedEventArgs e)
+        private ObservableCollection<Contact> _contacts;
+
+        void Call_Clicked(object sender, System.EventArgs e)
         {
-            var contact = e.SelectedItem as Contact;
-            DisplayAlert("Selected", contact.Name, "OK");
+            var menuItem = sender as MenuItem;
+            var contact = menuItem.CommandParameter as Contact;
+
+            DisplayAlert("Clicked", contact.Name, "OK");
         }
 
-        void Handle_ItemTapped(object sender, Xamarin.Forms.ItemTappedEventArgs e)
+        void Delete_Clicked(object sender, System.EventArgs e)
         {
-            var contact = e.Item as Contact;
-            DisplayAlert("Tapped", contact.Name, "OK");
+            var contact = (sender as MenuItem).CommandParameter as Contact;
+            _contacts.Remove(contact);
+            //DisplayAlert("Clicked", contact.Name, "OK");
         }
+
+        ObservableCollection<Contact> GetContacts()
+        {
+            return new ObservableCollection<Contact>
+            {
+                new Contact { Name="Lucas", ImageUrl="http://lorempixel.com/100/100/people/1" },
+                new Contact { Name = "Sierra", ImageUrl = "http://lorempixel.com/100/100/people/2", Status="Hey Let's talk" }
+            };
+        }
+
+        void Handle_Refreshing(object sender, System.EventArgs e)
+        {
+            listView.ItemsSource = GetContacts();
+        }
+
+        //void Handle_ItemSelected(object sender, Xamarin.Forms.SelectedItemChangedEventArgs e)
+        //{
+        //    var contact = e.SelectedItem as Contact;
+        //    DisplayAlert("Selected", contact.Name, "OK");
+        //}
+
+        //void Handle_ItemTapped(object sender, Xamarin.Forms.ItemTappedEventArgs e)
+        //{
+        //    var contact = e.Item as Contact;
+        //    DisplayAlert("Tapped", contact.Name, "OK");
+        //}
 
         public Selection()
         {
             InitializeComponent();
 
-            listView.ItemsSource = new List<Contact>
-            {
-                new Contact { Name="Lucas", ImageUrl="http://lorempixel.com/100/100/people/1" },
-                new Contact { Name = "Sierra", ImageUrl = "http://lorempixel.com/100/100/people/2", Status="Hey Let's talk" }
-            };
+            _contacts = GetContacts();
+
+            listView.ItemsSource = _contacts;
         }
     }
 }
