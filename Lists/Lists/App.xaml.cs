52ac4304608bc63288069ac21b9367a5017e3145
thegreatsantini <lss13@my.fsu.edu>
2018-10-22 12:28:13 -0700

    completed search bar section

--- Lists/Lists/App.xaml.cs
+++ Lists/Lists/App.xaml.cs
@@ -11,7 +11,7 @@ namespace Lists
         {
             InitializeComponent();
 
-            MainPage = new Selection();
+            MainPage = new SearchBar();
         }
 
         protected override void OnStart()
---
+++ Lists/Lists/SearchBar.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Lists.Models;
+using Xamarin.Forms;
+
+
+namespace Lists
+{
+    public partial class SearchBar : ContentPage
+    {
+        void Handle_TextChanged(object sender, Xamarin.Forms.TextChangedEventArgs e)
+        {
+            listView.ItemsSource = GetContacts(e.NewTextValue);
+        }
+
+        IEnumerable<Contact> GetContacts(string query=null)
+        {
+            var contacts = new List<Contact>
+            {
+                new Contact { Name="Lucas", ImageUrl="http://lorempixel.com/100/100/people/1" },
+                new Contact { Name = "Sierra", ImageUrl = "http://lorempixel.com/100/100/people/2", Status="Hey Let's talk" }
+            };
+
+            if (String.IsNullOrWhiteSpace(query))
+                return contacts;
+
+            return contacts.Where( contact => contact.Name.StartsWith(query));
+        }
+
+        public SearchBar()
+        {
+            InitializeComponent();
+
+            listView.ItemsSource = GetContacts();
+        }
+    }
+}
